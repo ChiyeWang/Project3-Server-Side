9c9af362d5e618f2e4a6adccb3f6048a7e0659a4
Kristina <tuf54356@temple.edu>
2018-03-13 04:38:03 -0400

    Added more aspx pages, and a datamanipulation class to the memberLibrary

---
+++ Project3/MembersLibrary/DataManipulation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembersLibrary
+{
+    class DataManipulation
+    {
+        //for the public static methods that will manipulate member info - such as updating database
+    }
+}
