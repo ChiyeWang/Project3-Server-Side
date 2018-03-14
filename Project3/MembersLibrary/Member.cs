01fb559ac4d592e7a0e4569dcdda768e0458eda7
Kristina <Kristina@KRISTINA>
2018-03-14 15:44:41 -0400

    Got back member class old code

--- Project3/MembersLibrary/Member.cs
+++ Project3/MembersLibrary/Member.cs
@@ -8,29 +8,32 @@ namespace MembersLibrary
 {
     public class Member
     {
-        public string userName;
-        private string password;
-        private string fullName; //
-        private string emailAddress;
+        public string userName { set; get; }
+        public string password { set; get; }
+        public string fullName { set; get; }
+        public string emailAddress { set; get; }
 
         //Public properties that everyone can see it
-
-        public string state;
-        public string description;
-        public string Address;
-        public string Email;
-        public string Age;
-        public string Height;
-        public string CommitmentType;
-        public string Likes;
+        //changed the case of the variable names for consistency! so all of them begin
+        //with a lower case
+        public string state { set; get; }
+        public string description { set; get; }
+        public string address { set; get; }
+        public string contactEmail { set; get; }
+        public string age { set; get; }
+        public string height { set; get; }
+        public string commitmentType { set; get; }
+        public string likes { set; get; }
+        public string profilePhoto { set; get; }
 
         //Private properties that only members can see
-        private string phoneNumber;
-        private string occupation;
-        private string favorites;
-        private string city;
+        public string phoneNumber { set; get; }
+        public string occupation { set; get; }
+        public string favorites { set; get; }
+        public string city { set; get; }
         //Private properties that keep track of the persons request status
-        private bool request;
+        //property to indicate whether or not their profile shows up
+        public bool show { set; get; }
 
         public Member(string UserName, string Password, string FullName, string EmailAddress)
         {
@@ -40,46 +43,88 @@ namespace MembersLibrary
             this.emailAddress = EmailAddress;
         }
 
+        //you can simplify propertiese of a class to shorthand {set;get;}!
+        //I kept the old code here
+        /*
+        public string UserName
+        {
+            get { return UserName; }
+            set { UserName = value; }
+        }
         public string Password
         {
             get { return password; }
             set { password = value; }
         }
-
         public string FullName
         {
             get { return fullName; }
             set { fullName = value; }
         }
-
         public string EmailAddress
         {
             get { return emailAddress; }
             set { emailAddress = value; }
         }
-
+        public string State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public string ContactEmail
+        {
+            get { return contactEmail; }
+            set { contactEmail = value; }
+        }
+        public string Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public string CommitmentType
+        {
+            get { return commitmentType; }
+            set { commitmentType = value; }
+        }
+        public string Likes
+        {
+            get { return likes; }
+            set { likes = value; }
+        }
         public string PhoneNumber
         {
             get { return phoneNumber; }
             set { phoneNumber = value; }
         }
-
         public string Occupation
         {
             get { return Occupation; }
             set { occupation = value; }
         }
-
         public string Favorites
         {
             get { return favorites; }
             set { favorites = value; }
         }
-
-        public bool Request
+        public string City
         {
-            get { return request; }
-            set { request = value; }
-        }
+            get { return city; }
+            set { city = value; }
+        }*/
     }
 }
